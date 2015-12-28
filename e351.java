e9e65e0eaee72e50cbbfeb7f937b543559eda347
hhchin <hhchin87@gmail.com>
2015-12-28 12:37:10 +0800

    solve 351 using sum of totient function

--- e351.java
+++ e351.java
@@ -2,92 +2,35 @@ import java.util.*;
 
 public class e351
 {
+    public static long N = 100000000;
+    //public static long N= 5;
+    public static HashMap<Long,Long> mem = new HashMap<Long, Long>();
     public static void main (String [] args)
     {
-        int order = 100000000;
-        //int order  = 5;
-        LinkedList<Pt> Q = new LinkedList<Pt>();
-        HashSet<Pt> hs = new HashSet<Pt> ();
-        long soln = 0;
-        Q.addLast(new Pt(1,1, order));
-        hs.add(Q.peek());
-        while(Q.size()>0)
-        {
-            Pt cur = Q.removeFirst();
-            soln++;
-            ArrayList<Pt> next = cur.move();
-            for(Pt p : next)
-            {
-                Pt rep = p.rep();
-                if(hs.contains(rep)) continue;
-                hs.add(rep);
-                Q.add(p);
-
-                //                System.out.println(p);
-            }
-        }
-
-        long lo = (long)(order);
-        long tot = 3*(lo+1)*lo+1;
+            
+        long cop = R(N);
+        long soln= (N-1)*(N)/2- cop;
+        soln+=(N-1);
+        soln*=6;
         System.out.println(soln);
-        System.out.println(tot);
-
-        System.out.println( tot-6*soln-1);
-
     }
-    static class Pt
+
+
+    private static long R(long n)
     {
-        int x, y, depth;
+        if(n==1) return 0L;
+        if(mem.containsKey(n)) return mem.get(n);
 
-        public Pt(int x, int y, int depth)
+        long soln = (n*(n-1))/2;
+        long m=2;
+        while(m<=n)
         {
-            this.x = x;
-            this.y = y;
-            this.depth = depth;
+            long rem = n/m;
+            long next = n/rem;
+            soln -= (next-m+1)*R(rem);
+            m = next+1;
         }
-
-        public String toString()
-        {
-            return String.format("(%d, %d, %d) ",x,y,depth);
-        }
-
-        public ArrayList<Pt> move()
-        {
-            ArrayList<Pt> next = new ArrayList<Pt>();
-            if(depth==1)
-                return next;
-            next.add(new Pt(x-1, y+1, depth-1));
-            next.add(new Pt(x+1, y+1, depth-1));
-            //next.add(new Pt(x+1, y, depth-1));
-            return next;
-        }
-
-        public int GCD(int a, int b)
-        {
-            if(a==0) return b;
-            return GCD(b%a, a);
-        }
-
-        public Pt rep()
-        {
-            if(x==0) return new Pt(0,2, depth);
-            int tx = x;
-            int ty = y;
-            int val = x<0 ? -1 : 1;
-            tx = tx*val;
-            int g = tx>ty ? GCD(tx,ty) : GCD(ty,tx);
-            return new Pt(val*tx/g, ty/g, depth);
-        }
-
-        public int hashCode()
-        {
-            return x*y;
-        }
-
-        public boolean equals(Object o)
-        {
-            Pt p = (Pt)o;
-            return p.x==x && p.y==y;
-        }
+        mem.put(n,soln);
+        return soln;
     }
 }
